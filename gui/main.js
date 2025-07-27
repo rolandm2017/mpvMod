abd289ea95134d9809f5e0bdf5dcdaeeb396b0b3
Roland Mackintosh <rolandmackintosh1@gmail.com>
2025-07-26 22:02:18 -0700

    return to the SRT wall thing

--- gui/main.js
+++ gui/main.js
@@ -1,64 +1,36 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const net = require('net');
-const mpvSocket = '\\\\.\\pipe\\mpvsocket';
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
-const mpvPath = process.env.MPV_PATH;
+  mainWindow.loadFile('index.html');
+  
+  // Open DevTools in development
+  if (process.argv.includes('--dev')) {
+    mainWindow.webContents.openDevTools();
+  }
+}
 
-const client = net.createConnection(mpvSocket, () => {
-  console.log('Connected to MPV');
-  client.write(JSON.stringify({
-    "command": ["observe_property", 1, "time-pos"]
-  }) + '\n');
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
-client.on('data', (data) => {
-  try {
-    const msg = JSON.parse(data.toString());
-    if (msg.event === "property-change" && msg.name === "time-pos") {
-      const currentTime = msg.data;
-      // send to renderer, or use to highlight subtitle
-    }
-  } catch (e) {
-    console.error("Failed to parse MPV response", e);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
 });
-
-
-
-// function createWindow() {
-//   const mainWindow = new BrowserWindow({
-//     width: 1400,
-//     height: 800,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       contextIsolation: false
-//     }
-//   });
-
-//   mainWindow.loadFile('index.html');
-  
-//   // Open DevTools in development
-//   if (process.argv.includes('--dev')) {
-//     mainWindow.webContents.openDevTools();
-//   }
-
-//   ipcRenderer.on()
-// }
-
-// app.whenReady().then(() => {
-//   createWindow();
-
-//   app.on('activate', () => {
-//     if (BrowserWindow.getAllWindows().length === 0) {
-//       createWindow();
-//     }
-//   });
-// });
-
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') {
-//     app.quit();
-//   }
-// });
