3a51748c54d1551c8cf960d8e068472f136bb877
Roland Mackintosh <rolandmackintosh1@gmail.com>
2025-08-01 17:00:04 -0700

    connect mp3 player state to surfer svelte component

---
+++ gui/tests/mp3PlayerState.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect, vi } from 'vitest';
+
+import { MP3PlayerState } from '$lib/utils/mp3PlayerState';
+
+describe('MP3PlayerState', () => {
+	it('keeps track of what is mounted', () => {
+		//
+	});
+});
